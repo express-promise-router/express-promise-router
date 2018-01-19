e2c5d7fd8f5acd20198838ae604379b6033004d4
Moritz Mahringer <me@moritzmahringer.de>
2018-01-19 22:04:43 +0100

    Fix code style

--- test/typescript.test.js
+++ test/typescript.test.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 describe('TypeScript', function() {
     it('should compile base-case successfully against index.d.ts', function(done) {
-        this.timeout(20000)
+        this.timeout(20000);
         tt.compile([path.resolve(__dirname + '/typescript-resources/base-case.ts')], {}, done.bind(null));
     });
 });
