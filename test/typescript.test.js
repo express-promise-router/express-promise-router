0af95db7786370801861b6426f78b13a8622c01d
Moritz Mahringer <me@moritzmahringer.de>
2018-01-24 18:33:38 +0100

    Rename path and file

--- test/typescript.test.js
+++ test/typescript.test.js
@@ -6,6 +6,6 @@ var path = require('path');
 describe('TypeScript', function() {
     it('should compile base-case successfully against index.d.ts', function(done) {
         this.timeout(20000);
-        tt.compile([path.resolve(__dirname + '/typescript-resources/base-case.ts')], {}, done.bind(null));
+        tt.compile([path.resolve(__dirname + '/typescript-resources/typescript-base-case.ts')], {}, done.bind(null));
     });
 });
