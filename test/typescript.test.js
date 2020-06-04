3acdb14a9bb270e2ae48c9ef378fbdf30d248e0e
Moritz Mahringer <me@moritzmahringer.de>
2020-06-04 08:33:04 +0200

    Switch to forked typescript-definition-tester

--- test/typescript.test.js
+++ test/typescript.test.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var tt = require("typescript-definition-tester");
+var tt = require("@mormahr/typescript-definition-tester");
 var path = require("path");
 var resolve = require("path").resolve;
 var assert = require("chai").assert;
@@ -8,46 +8,46 @@ var spawnTypeScript = require("./util/launch-utils").spawnTypeScript;
 var GET = require("./util/http-utils").GET;
 
 describe("TypeScript", function () {
-    it("should compile base-case successfully against index.d.ts", function (done) {
-        this.timeout(20000);
-        tt.compile(
-            [
-                path.resolve(
-                    __dirname + "/typescript-resources/typescript-base-case.ts",
-                ),
-            ],
-            {},
-            done.bind(null),
-        );
+  it("should compile base-case successfully against index.d.ts", function (done) {
+    this.timeout(20000);
+    tt.compile(
+      [
+        path.resolve(
+          __dirname + "/typescript-resources/typescript-base-case.ts",
+        ),
+      ],
+      {},
+      done.bind(null),
+    );
+  });
+
+  it("should run the example and respond", function (done) {
+    this.timeout(5000);
+    var ts_file = resolve(
+      __dirname,
+      "./test-resources/typescript-base-case.ts",
+    );
+    var target = spawnTypeScript(ts_file);
+    var called = false;
+
+    target.stdout.on("data", function (data) {
+      if (data.toString().indexOf("START") === -1) {
+        return;
+      }
+
+      GET("/").then(function () {
+        called = true;
+        target.kill("SIGINT");
+      });
     });
 
-    it("should run the example and respond", function (done) {
-        this.timeout(5000);
-        var ts_file = resolve(
-            __dirname,
-            "./test-resources/typescript-base-case.ts",
-        );
-        var target = spawnTypeScript(ts_file);
-        var called = false;
-
-        target.stdout.on("data", function (data) {
-            if (data.toString().indexOf("START") === -1) {
-                return;
-            }
-
-            GET("/").then(function () {
-                called = true;
-                target.kill("SIGINT");
-            });
-        });
-
-        target.stderr.on("data", function (data) {
-            console.error(data.toString());
-        });
-
-        target.on("close", function () {
-            assert(called);
-            done();
-        });
+    target.stderr.on("data", function (data) {
+      console.error(data.toString());
     });
+
+    target.on("close", function () {
+      assert(called);
+      done();
+    });
+  });
 });
