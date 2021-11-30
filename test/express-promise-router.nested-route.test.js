8eacecc0f9fc171edcfff3ce4764b89c3c27cb89
Moritz Mahringer <me@moritzmahringer.de>
2021-11-30 19:34:18 +0000

    Add integration test with express-list-endpoints

--- test/express-promise-router.nested-route.test.js
+++ test/express-promise-router.nested-route.test.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var assert = require("chai").assert;
+var list = require("express-list-endpoints");
 
 var PromiseRouter = require("../lib/express-promise-router.js");
 
@@ -13,5 +14,21 @@ describe("nested routes", () => {
     router.use("/api", subRouter);
 
     assert.exists(router.stack[0].handle.stack);
+  });
+
+  it("should list routes with express-list-endpoints", () => {
+    const router = PromiseRouter();
+    const subRouter = PromiseRouter();
+
+    subRouter.get("/users", (req, res) => res.send("ok"));
+    router.use("/api", subRouter);
+
+    assert.deepEqual(list(router), [
+      {
+        path: "/api/users",
+        methods: ["GET"],
+        middlewares: ["anonymous"],
+      },
+    ]);
   });
 });
